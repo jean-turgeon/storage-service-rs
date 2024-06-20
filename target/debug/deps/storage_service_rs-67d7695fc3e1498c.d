53231a8f64dfa38f4d2d9f357c71c57ad46c8db7
jean-turgeon <jean_turgeon@ymail.com>
2024-06-20 09:32:32 -0300

    clean up

--- target/debug/deps/storage_service_rs-67d7695fc3e1498c.d
+++
@@ -1,5 +0,0 @@
-/home/jean/Documents/workspace/storage-service-rs/target/debug/deps/libstorage_service_rs-67d7695fc3e1498c.rmeta: src/main.rs
-
-/home/jean/Documents/workspace/storage-service-rs/target/debug/deps/storage_service_rs-67d7695fc3e1498c.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/storage_service_rs-eaabd5bb924035ec.d
+++
@@ -1,5 +0,0 @@
-/home/jean/Documents/workspace/storage-service-rs/target/debug/deps/libstorage_service_rs-eaabd5bb924035ec.rmeta: src/main.rs
-
-/home/jean/Documents/workspace/storage-service-rs/target/debug/deps/storage_service_rs-eaabd5bb924035ec.d: src/main.rs
-
-src/main.rs:
